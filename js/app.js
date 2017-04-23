d90b1c06d307e5de983ef3a3410d3e7d63df2aa7
MaryoryChavez <maryory.chavez@gmail.com>
2017-04-23 17:43:11 -0500

    header 50%

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-$(document).foundation();
+//$(document).foundation();
+
+$(document).ready(init);
+
+function init() {
+    tachadoMenu();
+}
+
+function tachadoMenu() {
+    console.log("tachado");
+    $('#ingles').click(function () {
+        $(this).addClass('tachar');
+        $('#español').removeClass('tachar');
+    });
+    $('#español').click(function () {
+        $(this).addClass('tachar');
+        $('#ingles').removeClass('tachar');
+    });
+}
